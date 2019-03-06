feb951143c0f79567690fdbfc4b20fda1d1153e9
Andi Herlan <herlandi.27@gmail.com>
2019-03-06 08:17:32 +0700

    tambah file nc4 dan skrip impornya

--- Tutorial_NetCDFtoDF.R
+++ Tutorial_NetCDFtoDF.R
@@ -2,16 +2,18 @@ library(RNetCDF)
 library(dplyr)
 
 # impor data dari file netcdf
-file <- "~/R/hujan/raw/"
+file <- "~/R/hujan/raw/trmm/3B42_Daily.20090101.7.nc4"
 fid <- open.nc(file)
 print.nc(fid)
 data <- read.nc(fid)
 
 # dapatkan nilai hujan dan koordinat
-prc <- data$precipitation
-xlon <- as.character(data$nlon)
-ylat <- as.character(data$nlat)
+prc <- as.data.frame(data$precipitation)
+xlon <- as.character(data$lon)
+ylat <- as.character(data$lat)
 
 # menamai baris dan kolom
 names(prc) <- xlon
 row.names(prc) <- ylat
+
+prc
