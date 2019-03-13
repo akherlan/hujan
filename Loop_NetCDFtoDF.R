c38cbd9e32c5b02a449d8a74848f51c2bf05eafb
Andi Herlan <herlandi.27@gmail.com>
2019-03-13 18:42:00 +0700

    modifikasi skrip NetCDFtoDF dan spatialRaster

--- Loop_NetCDFtoDF.R
+++ Loop_NetCDFtoDF.R
@@ -2,7 +2,13 @@ library("RNetCDF")
 library("dplyr")
 library("tidyr")
 
-daftar <- list.files(path = "~/Desktop/rain", pattern = "nc4", full.names = TRUE)
+# fungsi ambil tanggal dari nama file netcdf
+substrTgl <- function(x, n){substr(x, nchar(x)-n, nchar(x)-n+7)}
+
+# proses looping
+lokasidata <- "~/R/hujan/raw/rain" # ganti nama folder tempat menyimpan data netcdf
+daftar <- list.files(path = lokasidata, pattern = "nc4", full.names = TRUE)
+text <- list.files(path = lokasidata, pattern = "nc4", full.names = FALSE)
 list_prc <- vector("list", length(daftar))
 names(list_prc) <- daftar
 for(i in daftar){
@@ -10,10 +16,16 @@ for(i in daftar){
   prc <- as.data.frame(data$precipitationCal)
   names(prc) <- as.character(data$lon)
   row.names(prc) <- as.character(data$lat)
-  prc <- mutate(prc, lat=row.names(prc))
+  prc <- mutate(prc, lat = row.names(prc))
   prc <- gather(prc, "lon", "prc", 1:5)
+  # tambah tanggal
+  text <- i
+  tgl <- substrTgl(text, 35)
+  prc <- mutate(prc, tgl = as.Date(paste(tgl), format = "%Y%m%d"))
+  # masukkan data satu file ke dalam satu list
   list_prc[[i]] <- prc
 }
 
+# simpan data dalam tabel csv
 prc_all <- bind_rows(list_prc)
 write.csv(prc_all, file = "prc.csv", row.names = FALSE)
--- Tutorial_SpatialRaster.R
+++ Tutorial_SpatialRaster.R
@@ -7,7 +7,7 @@ library("raster")
 library("RNetCDF")
 
 # impor dan baca file netcdf
-input <- "~/R/hujan/raw/rain/3B-DAY-E.MS.MRG.3IMERG.20140312-S000000-E235959.V05.nc4.nc4"
+input <- "~/R/hujan/raw/rain/3B-DAY-E.MS.MRG.3IMERG.20140312-S000000-E235959.V05.nc4.nc4" # ganti alamat datanya
 file <- open.nc(input)
 nc <- read.nc(file)
 
@@ -20,3 +20,18 @@ proj <- CRS("+proj=longlat +datum=WGS84")
 prc <- raster(nc$precipitationCal)
 extent(prc) <- c(min(lon), max(lon), min(lat), max(lat))
 proj4string(prc) <- proj
+plot(prc)
+
+# plot titik stasiun hujan
+library("dplyr")
+sta <- read.csv('sta.csv', header = TRUE) # ganti alamat file csv-nya
+stasort <- slice(sta, c(1,10,12,14))
+stapoin <- SpatialPoints(cbind(stasort$lon,stasort$lat),proj4string = CRS('+proj=longlat +datum=WGS84'))
+plot(stapoin, pch=16, col="red", add = TRUE)
+
+# plot batas DAS
+library("rgdal")
+shp <- "~/R/hujan/basin/hybas 9/hybas_lake_au_lev09_v1c.shp" # ganti alamat file shp-nya
+shp <- readOGR(shp)
+shp <- spTransform(shp, proj)
+plot(shp, bg="transparent", add=TRUE)
